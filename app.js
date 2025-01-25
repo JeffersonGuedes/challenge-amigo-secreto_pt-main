1b48b27a5ac16bb5757f80b5625b0d5e80cdf8bf
jefferson guedes <jefferson.guedes@sepog.fortaleza.ce.gov.br>
2025-01-25 16:56:55 -0300

    :tada: Feat: Primeiro teste do Challenge Amigo secreto

---
+++ app.js
@@ -0,0 +1,19 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// function adicionarAmigo(){
+
+// }
+let Friends = [];
+const lista = document.getElementById('listaAmigos');
+const resultados = document.getElementById('resultado');
+function adicionarAmigo() {
+    const inputFriends = document.getElementById('amigo').value;
+    if (!inputFriends || typeof(inputFriends) === "number") {
+        console.log("Nome de amigo inválido!")   
+    } else {
+        console.log("chegou")
+    }
+}
+
+function sortearAmigo() {
+
+}
