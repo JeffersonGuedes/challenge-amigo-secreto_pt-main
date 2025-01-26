b2debc1d5cdc08452cef051d1a80c3ab934d950d
jefferson guedes <jefferson.guedes@sepog.fortaleza.ce.gov.br>
2025-01-25 22:03:32 -0300

    :sparkles: Feat: Foi Finalizado o Challenge - foi modificado alguns detalhes do css e acrescentado novos estilos - foi criado outras funções, modificado o input e colocado no forms - criação de nova função de limpar listas - e criação de validações para evitar erros

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-// function adicionarAmigo(){
+let friends = [];
+let regex = /^[A-Za-z]+$/;
+const list = document.getElementById('listaAmigos');
+const result = document.getElementById('resultado');
+const btn = document.getElementById('btn-submit');
+let inputFriends;
 
-// }
-let Friends = [];
-const lista = document.getElementById('listaAmigos');
-const resultados = document.getElementById('resultado');
-function adicionarAmigo() {
-    const inputFriends = document.getElementById('amigo').value;
-    if (!inputFriends || typeof(inputFriends) === "number") {
-        console.log("Nome de amigo inválido!")   
+const adicionarAmigo = () => {
+    inputFriends = document.getElementById('amigo').value;
+    if (!inputFriends || !regex.test(inputFriends)) {
+        if(!isNaN(inputFriends)){
+            alert("Você digitou número ao invés do nome. Por favor, insira um nome.");
+            inputClear();
+        } else {
+            alert("Por favor, insira um nome.");
+            inputClear(); 
+        }
     } else {
-        console.log("chegou")
+        console.log('teste')
+        friends.push(inputFriends);
+        inputClear();
+        updatedList();
     }
 }
 
-function sortearAmigo() {
-
+const sortearAmigo = () => {
+    result.innerHTML = '';
+    const amount = friends.length;
+    let li = document.createElement('li');
+    if(amount >= 2){
+        const luck = Math.floor(Math.random() * amount);
+        li.textContent = `O sorteado foi ${friends[luck]}`;
+        result.appendChild(li);
+    } else {
+        li.textContent = `Não foi possível sortear, mínimo para sorteo é 2 pessoas!`
+        result.appendChild(li);
+        li.classList.add('name-danger');
+    }
 }
+
+const inputClear = () => {
+    document.getElementById('amigo').value = '';
+}
+
+const listClear = () => {
+    list.innerHTML = '';
+    friends = [];
+    result.innerHTML = '';
+}
+
+const updatedList = () => {
+    list.innerHTML = ''
+    console.log(friends)
+    friends.map(function(index){
+        let li = document.createElement('li');
+        li.textContent = index;
+        list.appendChild(li);
+    })
+}
+
+btn.addEventListener('click', function(event){
+    event.preventDefault();
+})
+
